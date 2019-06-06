2ee4de22e889ca7b024fb9b6729371f97edbde3a
Di <849131492@qq.com>
2019-06-06 19:40:21 +0800

    删除Swift test 模块

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -13,16 +13,6 @@ module Pod
 
     def perform
       keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
-
-      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
-      case framework
-        when :quick
-          configurator.add_pod_to_podfile "Quick', '~> 1.2.0"
-          configurator.add_pod_to_podfile "Nimble', '~> 7.0.2"
-          configurator.set_test_framework "quick", "swift", "swift"
-
-        when :none
-          configurator.set_test_framework "xctest", "swift", "swift"
       end
 
       snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym

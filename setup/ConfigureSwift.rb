9e5911e55589d04469112f175b733353a4aee266
Di <849131492@qq.com>
2019-06-06 20:36:19 +0800

    删除询问项

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -12,22 +12,7 @@ module Pod
     end
 
     def perform
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
-
-      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
-      case snapshots
-        when :yes
-          configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
-
-          if keep_demo == :no
-              puts " Putting demo application back in, you cannot do view tests without a host application."
-              keep_demo = :yes
-          end
-
-          if framework == :quick
-              configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 6.3.0"
-          end
-      end
+      keep_demo = "yes"
 
       Pod::ProjectManipulator.new({
         :configurator => @configurator,

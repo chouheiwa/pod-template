ad0e3dca1af63eba29c3b638a6634f41bd43937f
chouheiwa <849131492@qq.com>
2019-06-19 14:23:27 +0800

    给demo项目中增加网络请求抓包接口

--- templates/swift/Example/PROJECT/AppDelegate.swift
+++ templates/swift/Example/PROJECT/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import netfox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        NFX.sharedInstance().start()
         // Override point for customization after application launch.
         return true
     }
